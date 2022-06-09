5eead29f62d10915b4d6c2b954db1f15a0299cae
bulldoguk <57550309+bulldoguk@users.noreply.github.com>
2022-06-08 21:17:00 -0500

    Fixed so user keeps same GUID

--- modules/user/actions.py
+++ modules/user/actions.py
@@ -8,9 +8,6 @@ def add_or_update(info):
         user_collection = get_db().users
         # must always have extended_info: True when we have our own user record
         info["extended_info"] = True
-        if not info.get("guid"):
-            info["guid"] = str(uuid.uuid4())
-        print(f'got user', info)
         record = user_collection.find_one_and_update(
             {"email": info.get("email")},
             {"$set": info},
@@ -19,7 +16,23 @@ def add_or_update(info):
         # All member of the myhmbiz.com home domain are superusers here automatically
         if record.get("hd") == 'myhmbiz.com':
             record["superAdmin"] = True
-        return record
+        else:
+            record["superAdmin"] = False
+        # if this is a new user, we need to set a unique GUID
+        if not record.get("guid"):
+            finalRecord = user_collection.find_one_and_update(
+                {"email": record.get("email")},
+                {"$set":
+                    {
+                        "guid": str(uuid.uuid4()),
+                        "superAdmin": record.get("superAdmin")
+                    }
+                },
+                return_document=ReturnDocument.AFTER
+            )
+            return finalRecord
+        else:
+            return record
     except Exception as e:
         print('Failed to update user record', e)
         return None
