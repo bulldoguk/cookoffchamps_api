06cb2c1d9f156461eb32c44295ef4a9e3a7835fe
bulldoguk <gary@myhmbiz.com>
2022-09-05 06:19:42 -0500

    Mongo client connection string

--- modules/db/mongodb.py
+++ modules/db/mongodb.py
@@ -7,7 +7,9 @@ from modules.db.schema import template
 from pymongo import MongoClient
 
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient('mongodb+srv://cookoffchamps_api:dQBlGRuVkj1OGzD6@cluster0-pri.gcoozxh.mongodb.net/cookoffchamps?retryWrites=true&w=majority')
+# client = MongoClient('mongodb+srv://cookoffchamps_api:dQBlGRuVkj1OGzD6@cluster0-pri.gcoozxh.mongodb.net/cookoffchamps?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://cookoffchamps_api:dQBlGRuVkj1OGzD6@cluster0-pri.gcoozxh.mongodb.net/?retryWrites=true&w=majority')
+# mongodb+srv://<username>:<password>@cluster0-pri.gcoozxh.mongodb.net/?retryWrites=true&w=majority
 db = client.cookoffchamps
 # Issue the serverStatus command and print the results
 serverStatusResult = db.list_collection_names()
